1735618099babb4e72580950248ede77a8be7d0f
wzdmriv <wzdmriv@gmail.com>
2020-04-02 17:36:19 +0900

    1

--- schedule.js
+++ schedule.js
@@ -1,27 +1,7 @@
-var Events = {
-"monthly": [
-{
-"id": 1,
-"name": “予定”,
-"startdate": "2018-08-27",
-"enddate": "",
-"color": "#e84d38"
-},
-{
-"id": 2,
-"name": "予定2",
-"startdate": "2018-08-28",
-"enddate": "",
-"color": "#53c3db"
-}
-]
-};
- 
 $(window).load( function() {
 $('#mycalendar').monthly({
-mode: 'event',
-dataType: 'json',
-events: sampleEvents,
+//mode: 'event',
+//dataType: 'json',
 eventList:false
 });
 });
