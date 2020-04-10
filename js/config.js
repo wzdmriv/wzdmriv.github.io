c892168a339042d6e7647ef1c8aa17e62a68ff58
wzdmriv <wzdmriv@gmail.com>
2020-04-10 22:17:37 +0900

    1

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBkd4VZq7pTR0VB7F_T5Trk7K4qdfrdQ8I",
-    authDomain: "res-demo-26a88.firebaseapp.com",
-    databaseURL: "https://res-demo-26a88.firebaseio.com",
-    projectId: "res-demo-26a88",
-    storageBucket: "res-demo-26a88.appspot.com",
-    messagingSenderId: "108879459276",
-    appId: "1:108879459276:web:71bc9ef5fa80fc7a386369"
-};
+    apiKey: "AIzaSyChLNUEiDdDCHOZhF0wgcQeVjs7t7Romns",
+    authDomain: "pomp-calendar.firebaseapp.com",
+    databaseURL: "https://pomp-calendar.firebaseio.com",
+    projectId: "pomp-calendar",
+    storageBucket: "pomp-calendar.appspot.com",
+    messagingSenderId: "177811656170",
+    appId: "1:177811656170:web:ebbf849a5b63f7ee1ff693"
+  };
