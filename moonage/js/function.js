e9968ad38319069e0bbdc1bb06695336fd9a3b16
wzdmriv <wzdmriv@gmail.com>
2020-04-28 03:11:54 +0900

    1

--- moonage/js/function.js
+++ moonage/js/function.js
@@ -8,3 +8,26 @@ function centeringOrbit() {
 
     $( window ).resize( centeringOrbit ) ;
 }
+
+function getMoonage(){
+    $.getJSON('https://mgpn2.sakura.ne.jp/api/moon/position.cgi?json&lat=35.0000&lon=140.0000')
+    .done(function(data) {
+        $('p').text( JSON.stringify(data) );
+        // 中身が空でなければ、その値を［住所］欄に反映
+        if (data.results) {
+            testbox = document.getElementById("testbox");
+            testbox.innerHTML = "成功";
+            //var result = data.results[0];
+          //testbox = document.getElementById("testbox");
+          //testbox.innerHTML = result;
+        // 中身が空の場合は、エラーメッセージを反映
+        } else {
+            testbox = document.getElementById("testbox");
+            testbox.innerHTML = "エラー";
+        }
+    })
+    .fail(function(){
+        testbox = document.getElementById("testbox");
+            testbox.innerHTML = "成功１";
+    });
+}
