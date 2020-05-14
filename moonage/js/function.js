e6b992dbcd6dc129d3134f4d8f8f5050037557b3
wzdmriv <wzdmriv@gmail.com>
2020-05-14 18:01:20 +0900

    1

--- moonage/js/function.js
+++ moonage/js/function.js
@@ -1,3 +1,46 @@
+var doc;
+var flag = 1;
+
+
+function setting(){
+    $(function(){
+        $("#screen").unbind().click(function(){
+            if (flag==1){
+                $(".layer1").fadeOut("fast",function(){
+                    $(".layer2").fadeIn("fast");
+                });
+                flag = 2
+            }else if (flag==2){
+                $(".layer2").fadeOut("fast", function(){
+                    $(".layer1").fadeIn("fast");
+                });
+                flag = 1;
+            }
+        });
+    })
+    getMoonage();
+    centeringOrbit();
+
+}
+
+
+function getMoonage(){
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://mgpn2.sakura.ne.jp/api/moon/position.cgi?lat=35.0000&lon=140.0000', true);
+ 
+    request.onload = function () {
+      var data = this.response;
+      console.log(data);
+      const parser = new DOMParser();
+      doc = parser.parseFromString(data, "text/xml");
+      testbox = document.getElementById("testbox");
+      var con = doc.getElementsByTagName('age');
+            testbox.innerHTML = con[0].textContent;
+    };
+    request.send();
+}
+
+
 function centeringOrbit() {
     //画面(ウィンドウ)の幅、高さを取得
     var w = $( window ).width() ;
@@ -9,38 +52,25 @@ function centeringOrbit() {
     $( window ).resize( centeringOrbit ) ;
 }
 
-function getMoonage(){
-    fetch('https://mgpn2.sakura.ne.jp/api/moon/position.cgi?json&lat=35.0000&lon=140.0000',{mode:'cors'})
-  .then(function(response) {
-    return response.text();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-  });
-    /*$.ajax({
-        type: "GET",
-        url: 'https://mgpn2.sakura.ne.jp/api/moon/position.cgi?json&lat=35.0000&lon=140.0000',
-        dataType:"json"
-    })
-    .done(function(data) {
-        $('p').text( JSON.stringify(data) );
-        // 中身が空でなければ、その値を［住所］欄に反映
-        if (data.results) {
-            testbox = document.getElementById("testbox");
-            testbox.innerHTML = "成功";
-            //var result = data.results[0];
-          //testbox = document.getElementById("testbox");
-          //testbox.innerHTML = result;
-        // 中身が空の場合は、エラーメッセージを反映
-        } else {
-            testbox = document.getElementById("testbox");
-            testbox.innerHTML = "エラー";
-        }
-    })
-    .error(function(XMLHttpRequest, textStatus, errorThrown) {
-    alert('error!!!');
-　　console.log("XMLHttpRequest : " + XMLHttpRequest.status);
-　　console.log("textStatus     : " + textStatus);
-　　console.log("errorThrown    : " + errorThrown.message);
-});*/
-}
+
+/*function changeBGColor(r, g, b) {
+    var img = new Image();
+    img.src = 'images/twinkling.png';
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+        img.style.display = 'none';
+    };
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for(var i = 0; i < (imageData.width*imageData.height); i++) {
+        imageData.data[i*4] = r;
+        imageData.data[i*4+1] = g;
+        imageData.data[i*4+2] = b;
+    }
+    ctx.putImageData(imageData, 0, 0);
+    var png = canvas.toDataURL();
+    var p = document.getElementById('twinkling');
+    p.style.background = png;
+    return;
+}*/
